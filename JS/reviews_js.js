6cd2bce2df975e739d28569906600d6a7040d5c0
Natan Kolodziej <natankolodziej@yahoo.com>
2023-04-29 13:35:01 +0100

    no jekyll added to fix error

--- JS/reviews_js.js
+++ JS/reviews_js.js
@@ -17,58 +17,39 @@ function storageavailable(type) { //exact copy from source.lab09 - data + persis
       storage.length !== 0;
   }
 }
-
 function reviewsubmition() {
-  // Get the values entered by the customer using the document.getElementById() method
-  var customername = document.getElementById("customername").value;
-  var contactdetails = document.getElementById("contact").value;
-  var reviewmessage = document.getElementById("review").value;
-
-  // Concatenate the variables together with newline characters in between
-  var message = "Name:" + customername + "\n" + "Contact:" + contactdetails + "\n" + "Review:" + reviewmessage + "\n";
-
-  // Display the concatenated message in the "currentreviews" element
-  var currentreviews = document.getElementById("currentreviews");
-  currentreviews.innerHTML += message + "<br>";
-  setlocalstorage(message)
-     location.reload();
+  var customername = document.getElementById("customername").value; //the correct input field is found 
+  var contactdetails = document.getElementById("contact").value; //the correct input field is found 
+  var reviewmessage = document.getElementById("review").value; //the correct input field is found 
+  var message = "Name:" + customername + "\n" + "Contact:" + contactdetails + "\n" + "Review:" + reviewmessage + "\n"; //message concatonated 
+  var currentreviews = document.getElementById("currentreviews"); //the paragraph element is found 
+  currentreviews.innerHTML += message + "<br>"; //the paragraph element is altered to contain the message 
+  setlocalstorage(message)// the setlocal storage function is called and the message is passed
+     location.reload(); //the page is refreshed 
 }
-
-
-
 function setlocalstorage(contents) {
-  if (storageavailable('localStorage')) {
-    var existingcontent= localStorage.getItem('currentcontents')
-    if (existingcontent) {
-      contents = existingcontent + '\n' + contents;
+  if (storageavailable('localStorage')) { //checks whether or not local storage is available 
+    var existingcontent= localStorage.getItem('currentcontents') //checks whether or not currentcontents already exists in local storage and sets it to existing content. otherwise existing content is null
+    if (existingcontent) { // if existing content is not null
+      contents = existingcontent + '\n' + contents; // the message is added to the contents 
     }
-    localStorage.setItem('currentcontents', contents);
+    localStorage.setItem('currentcontents', contents); //the contents are added to local storage under the name of current contents 
   }
   else {
-    alert("Local Storage is not available, the review was not saved")
+    alert("Local Storage is not available, the review was not saved") //if local storage is not available then the user is alerted that their review was not submitted 
   }
 }
-
-window.addEventListener("load", function() {
-  clear();
-});
-
-window.addEventListener("load", function() {
+window.addEventListener("load", function() { //on load the road reviews function is called 
   loadreviews();
 });
-
+  
 function loadreviews() {
-  var currentreviews = document.getElementById("currentreviews");
-  var content = localStorage.getItem("currentcontents");
-  if (content) {
-    currentreviews.innerText = content;
+  var currentreviews = document.getElementById("currentreviews"); //find the current reviews paragraph 
+  var content = localStorage.getItem("currentcontents"); // checks local storage for current contents
+  if (content) { //if current content already exists 
+    currentreviews.innerText = content; // the paragraph is updated with the content 
   }
 }
-
-function clear() {
-  localStorage.clear();
-}
-
 // function updatefile() {
 //   const owner = "NatanK2039";
 //   const repo = "NatanK2039/Restaurant";
